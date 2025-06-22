be3322e263882cf740dc293bd538af220ef7babc
22f3000626 <shivasanthosh0804@gmail.com>
2025-06-22 11:02:52 +0530

    improved database and models.py

--- models.py
+++ models.py
@@ -13,7 +13,6 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     communities = db.relationship('CommunityMembership', back_populates='user', cascade="all, delete")
     posts = db.relationship('Post', backref='author', lazy=True)
-
     def __repr__(self):
         return f'<User {self.username}>'
 class Community(db.Model):
@@ -22,7 +21,6 @@ class Community(db.Model):
     description = db.Column(db.Text, nullable=True)
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
     creator = db.relationship('User', backref='created_communities', foreign_keys=[created_by])
     members = db.relationship('CommunityMembership', back_populates='community', cascade="all, delete")
     posts = db.relationship('Post', backref='community', lazy=True)
@@ -34,10 +32,8 @@ class CommunityMembership(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
     joined_at = db.Column(db.DateTime, default=datetime.utcnow)
-
     user = db.relationship('User', back_populates='communities')
     community = db.relationship('Community', back_populates='members')
-
     def __repr__(self):
         return f'<Membership User:{self.user_id} -> Community:{self.community_id}>'
 class Post(db.Model):
@@ -49,15 +45,12 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=False)
     likes = db.relationship('PostLike', backref='post', lazy='dynamic', cascade="all, delete-orphan")
-
     @property
     def like_count(self):
         return self.likes.filter_by(is_like=True).count()
-
     @property
     def dislike_count(self):
         return self.likes.filter_by(is_like=False).count()
-
     def __repr__(self):
         return f'<Post {self.id} by User:{self.user_id} in Community:{self.community_id}>'
 class PostLike(db.Model):
@@ -65,7 +58,6 @@ class PostLike(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
     is_like = db.Column(db.Boolean, nullable=False)
-
     user = db.relationship('User', backref='post_likes')
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -73,9 +65,7 @@ class Comment(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-
     author = db.relationship('User', backref='comments')
     post = db.relationship('Post', backref='comments')
-
     def __repr__(self):
         return f'<Comment {self.id} by User:{self.user_id}>'
